20bd265ff5aec8b72dc4a518189fcffebce22bd3
Rafael239 <43891004+Rafael239@users.noreply.github.com>
2018-10-17 15:13:39 -0300

    Jogo Atual

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1539799844,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fundo.png",
+		"images/plataforma-sheet0.png",
+		"images/bloco1-sheet0.png",
+		"images/bloco2-sheet0.png",
+		"images/bloco3-sheet0.png",
+		"images/maça-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/particles.png",
+		"images/btnplay-sheet0.png",
+		"images/btnclose-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/esquerda-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
